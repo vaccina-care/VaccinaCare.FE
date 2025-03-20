2ba84844da2521caf0c2b7334ddd05df12dded0e
Ch1mpleo <vietanh051203@gmail.com>
2025-03-21 01:11:21 +0700

    minor changes

--- src/types/framer-motion.d.ts
+++ src/types/framer-motion.d.ts
@@ -1,13 +1,13 @@
-declare module "framer-motion" {
-    export interface AnimatePresenceProps {
-        children?: React.ReactNode
-        mode?: "sync" | "wait" | "popLayout"
-        initial?: boolean
-        onExitComplete?: () => void
-        exitBeforeEnter?: boolean
-        presenceAffectsLayout?: boolean
-    }
+// declare module "framer-motion" {
+//     export interface AnimatePresenceProps {
+//         children?: React.ReactNode
+//         mode?: "sync" | "wait" | "popLayout"
+//         initial?: boolean
+//         onExitComplete?: () => void
+//         exitBeforeEnter?: boolean
+//         presenceAffectsLayout?: boolean
+//     }
 
-    export const AnimatePresence: React.FC<AnimatePresenceProps>
-}
+//     export const AnimatePresence: React.FC<AnimatePresenceProps>
+// }
 
